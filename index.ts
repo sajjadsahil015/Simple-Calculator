5beeff4726761daec807196cd9a3c22501701030
Sajjad <sajjadsahil015@gmail.com>
2024-03-23 22:30:19 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+  {
+    message: "Select one of the following opertor",
+    type: "list",
+    name: "Operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// Conditional Statements
+
+if (answer.Operator === "Addition") {
+  console.log(answer.FirstNumber + answer.SecondNumber);
+} else if (answer.Operator === "Subtraction") {
+  console.log(answer.FirstNumber - answer.SecondNumber);
+} else if (answer.Operator === "Multiplication") {
+  console.log(answer.FirstNumber * answer.SecondNumber);
+} else if (answer.Operator === "Division") {
+  console.log(answer.FirstNumber / answer.SecondNumber);
+} else{
+  console.log("Please enter the valid operator");
+    
+}
