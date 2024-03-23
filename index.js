a8f8d18bc36c9b10e0fcbce7516042edb09aefa2
Sajjad <sajjadsahil015@gmail.com>
2024-03-23 22:38:14 +0500

    new changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const answer = await inquirer.prompt([
     { message: "Enter First Number", type: "number", name: "FirstNumber" },
     { message: "Enter Second Number", type: "number", name: "SecondNumber" },
     {
-        message: "Select one of the following opertor",
+        message: "Select one of the following opertors",
         type: "list",
         name: "Operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
